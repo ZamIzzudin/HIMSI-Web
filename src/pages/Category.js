984b0d2182ba392292197aafb145736a88ae4a53
zahrasabila <zahra.sabila22@mhs.uinjkt.ac.id>
2022-12-08 09:39:04 +0700

    menambah page program kerja

---
+++ src/pages/Category.js
@@ -0,0 +1,30 @@
+export default function Category() {
+  return (
+    <div>
+      <h1>Kategori Kegiatan HIMSI</h1>
+      <ul className="Category">
+        <li>
+          <link to="/">Semua</link>
+        </li>
+        <li>
+          <link to="/">BPH</link>
+        </li>
+        <li>
+          <link to="/">Bidang PSDM</link>
+        </li>
+        <li>
+          <link to="/">Bidang Kominfo</link>
+        </li>
+        <li>
+          <link to="/">Bidang Dikti</link>
+        </li>
+        <li>
+          <link to="/">Bidang URT</link>
+        </li>
+        <li>
+          <link to="/">Bidang Adkesma</link>
+        </li>
+      </ul>
+    </div>
+  );
+}
