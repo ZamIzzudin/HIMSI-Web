e91d7f55ff1daf230f45929da61a190f5d578ec7
ZamIzzudin <azzamizzudinhasan@gmail.com>
2022-12-15 14:10:12 +0700

    add Category Raw Component

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -6,8 +6,6 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "../styles/pages/Category.css";
 
-import Sonnet from "../components/Sonnet";
-
 function Category() {
   return (
     // Bagian List Kegiatan
