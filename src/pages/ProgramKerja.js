4e1260f5d9953f7facea3ef9ad5b4d0a3db62085
zahrasabila <zahra.sabila22@mhs.uinjkt.ac.id>
2022-12-28 21:45:05 +0700

    menambahkan page proker

---
+++ src/pages/ProgramKerja.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import EventItem from "../components/EventItem";
+import Pagination from "react-bootstrap/Pagination";
+
+import "../styles/pages/ProgramKerja.css";
+
+const ProgramKerja = () => {
+  const detailEvents = [
+    {
+      tag: "ADKESMA",
+      title: "Meet Up 13.0",
+      date: "01/12/21",
+      deskripsi:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. ",
+      img: "../assets/img/meetup.png",
+      id: "1",
+    },
+    {
+      tag: "PSDM",
+      title: "Meet Up 13.0",
+      date: "01/12/21",
+      deskripsi:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. ",
+      img: "../assets/img/meetup.png",
+      id: "10",
+    },
+    {
+      tag: "DIKTI",
+      title: "Meet Up 13.0",
+      date: "01/12/21",
+      deskripsi:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. ",
+      img: "../assets/img/meetup.png",
+      id: "3",
+    },
+  ];
+
+  return (
+    <div className="program-kerja">
+      <Container>
+        <div className="search-bar">
+          <form>
+            <input type="text" name="" placeholder="Search Proker" />
+            <input type="submit" name="" value="Search" />
+            <div className="clear"></div>
+          </form>
+        </div>
+        <div className="eventList">
+          <div className="clear"></div>
+          <Row className="blank-space"></Row>
+          <Row className="judul text-center">
+            <Col>
+              <h1>Program Kerja</h1>
+            </Col>
+            <Row className="blank-space"></Row>
+          </Row>
+          {detailEvents.map((detailEvent, index) => {
+            return <EventItem detailEvent={detailEvent} index={index} />;
+          })}
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default ProgramKerja;
--- src/router.js
+++ src/router.js
@@ -1,42 +1,42 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 // Navbar
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 
 // Pages
-import Home from './pages/Home'
+import Home from "./pages/Home";
 import Page404 from "./pages/Page404";
 import Event from "./pages/Event";
 import EventDetail from "./pages/EventDetail";
 import Article from "./pages/Article";
 import ArticleDetail from "./pages/ArticleDetail";
 import Profile from "./pages/Profile";
-import Category from "./pages/Category";
+import ProgramKerja from "./pages/ProgramKerja";
 
 export default function Router() {
-    return (
-        <BrowserRouter>
-            <Navbar />
-            <Switch>
-                <Route exact path="/" component={Home} />
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
 
-                {/* Page Profile */}
-                <Route path="/profile" component={Profile} />
+        {/* Page Profile */}
+        <Route path="/profile" component={Profile} />
 
-                {/* Page Proker */}
-                <Route path="/program-kerja" component={Category} />
+        {/* Page Proker */}
+        <Route path="/program-kerja" component={ProgramKerja} />
 
-                {/* Page Event */}
-                <Route path="/event" component={Event} />
-                <Route path="/event-detail/:id" component={EventDetail} />
+        {/* Page Event */}
+        <Route path="/event" component={Event} />
+        <Route path="/event-detail/:id" component={EventDetail} />
 
-                {/* Page Article */}
-                <Route path="/article" component={Article} />
-                <Route path="/article-detail/:id" component={ArticleDetail} />
+        {/* Page Article */}
+        <Route path="/article" component={Article} />
+        <Route path="/article-detail/:id" component={ArticleDetail} />
 
-                {/* 404 Handle Page */}
-                <Route path="*" component={Page404} />
-            </Switch>
-            {/* <EventDetail /> */}
-        </BrowserRouter>
-    )
+        {/* 404 Handle Page */}
+        <Route path="*" component={Page404} />
+      </Switch>
+      {/* <EventDetail /> */}
+    </BrowserRouter>
+  );
 }
